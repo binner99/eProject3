3f23954c3111dded726f8aeb18134bab4fcef290
mimi1108 <45484459+mimi1108@users.noreply.github.com>
2019-01-07 11:48:47 +0700

    Update Admin.cs

--- Source Code/MobileService/Data/Admin.cs
+++ Source Code/MobileService/Data/Admin.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data
 {
+    [DataContract]
     class Admin
     {
+        [DataMember]
+        [Key]
+        [Display(Name = "User name")]
+        [StringLength(50, MinimumLength = 3)]
+        public string adName { get; set; }
+
+        [DataMember]
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 6)]
+        [Display(Name = "Password")]
+        public string adPass { get; set; }
+
+        [DataMember]
+        [Required]
+        [Display(Name = "Role")]
+        public bool adRole { get; set; }
+
+        [DataMember]
+        [DataType(DataType.Upload)]
+        public string adImage { get; set; }
+
     }
 }
