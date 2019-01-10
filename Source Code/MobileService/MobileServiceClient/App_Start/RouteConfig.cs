9aba093de609071b4797628b18237d293e3b7f7f
binner99 <binner99@users.noreply.github.com>
2019-01-10 13:27:45 +0700

    Client User

--- Source Code/MobileService/MobileServiceClient/App_Start/RouteConfig.cs
+++ Source Code/MobileService/MobileServiceClient/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MobileServiceClient
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "OR", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
