5b3558a602d1447aec4aa7ed882179d81ef540f9
binner99 <binner99@users.noreply.github.com>
2019-01-11 09:42:21 +0700

    Up

--- Source Code/MobileService/MobileServiceClient/Controllers/HomeController.cs
+++ Source Code/MobileService/MobileServiceClient/Controllers/HomeController.cs
@@ -1,17 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
+using Data;
+using MobileServiceClient.Models;
 
 namespace MobileServiceClient.Controllers
 {
     public class HomeController : Controller
     {
+        string url = "http://localhost:61560/api/Bill/";
+        HttpClient client = new HttpClient();
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index() => View();
+        
+        public PartialViewResult NextPay(string phone, double amount)
         {
-            return View();
+            TempData["billPhone"] = phone;
+            TempData["billTotal"] = amount;
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Bill(PaymentClient paymentClient)
+        {
+            var bill = new Bill
+            {
+                billCusName = paymentClient.Name,
+                billPhone = TempData["billPhone"].ToString(),
+                billTotal = Double.Parse(TempData["billTotal"].ToString()),
+                billDate = DateTime.Now,
+                billDes = "Recharge " + ViewBag.BillTotal
+            };
+            var model = client.PostAsJsonAsync<Bill>(url, bill).Result;
+            return RedirectToAction("Index");
         }
     }
 }
