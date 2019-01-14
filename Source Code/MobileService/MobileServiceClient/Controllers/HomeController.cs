775659ff421adc8444433b6dd336d5aa778d64df
binner99 <binner99@users.noreply.github.com>
2019-01-14 11:34:56 +0700

    Update

--- Source Code/MobileService/MobileServiceClient/Controllers/HomeController.cs
+++ Source Code/MobileService/MobileServiceClient/Controllers/HomeController.cs
@@ -10,13 +10,16 @@ namespace MobileServiceClient.Controllers
     {
         string url = "http://localhost:61560/api/Bill/";
         HttpClient client = new HttpClient();
+        public ActionResult Home() => View();
         // GET: Home
         public ActionResult Index() => View();
-        
-        public PartialViewResult NextPay(OR oR)
+        [HttpPost]
+        public PartialViewResult InfoBill(OR oR)
         {
-            TempData["billPhone"] = oR.Phone;
-            TempData["billTotal"] = oR.Amount;
+            Session["billPhone"] = oR.Phone;
+            Session["billTotal"] = oR.Amount;
+            ViewBag.billPhone = oR.Phone;
+            ViewBag.billTotal = oR.Amount;
             return PartialView();
         }
         [HttpPost]
@@ -25,10 +28,10 @@ namespace MobileServiceClient.Controllers
             var bill = new Bill
             {
                 billCusName = paymentClient.Name,
-                billPhone = TempData["billPhone"].ToString(),
-                billTotal = Double.Parse(TempData["billTotal"].ToString()),
+                billPhone = Session["billPhone"].ToString(),
+                billTotal = Double.Parse(Session["billTotal"].ToString()),
                 billDate = DateTime.Now,
-                billDes = "Recharge " + Double.Parse(TempData["billTotal"].ToString()) + " (₹)"
+                billDes = "Recharge " + Double.Parse(Session["billTotal"].ToString()) + " (₹)"
             };
             var model = client.PostAsJsonAsync<Bill>(url, bill).Result;
             return RedirectToAction("Index");
