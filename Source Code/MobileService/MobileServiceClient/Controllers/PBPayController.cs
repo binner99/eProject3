e8271f772f435ea605bf3538f677d2a6e043aa6e
mimi1108 <45484459+mimi1108@users.noreply.github.com>
2019-01-18 17:11:19 +0700

    poooooooooooooop

--- Source Code/MobileService/MobileServiceClient/Controllers/PBPayController.cs
+++ Source Code/MobileService/MobileServiceClient/Controllers/PBPayController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileServiceClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,21 @@ namespace MobileServiceClient.Controllers
 {
     public class PBPayController : Controller
     {
+        public PBPayController()
+        {
+            ViewBag.Current = "PBPay";
+        }
         // GET: PBPay
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public PartialViewResult NextPay(PBpay pBpay)
+        {
+            TempData["billTotal"] = pBpay.Amount;
+            TempData["billDes"] = pBpay.BillCode;
+            return PartialView();
         }
     }
 }
